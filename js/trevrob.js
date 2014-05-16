239742c94f9140e66e1d63fcd3378f627d1055fb
Trevor <TRobinson4@live.com>
2014-05-16 11:36:46 -0700

    first commit

---
+++ js/trevrob.js
@@ -0,0 +1,86 @@
+// Activates the Carousel
+
+$('[id="myCarousel1"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel1"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel2"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel2"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel3"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel3"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel4"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel4"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel5"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel5"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel6"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel6"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel7"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel7"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel8"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel8"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel9"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel9"]').carousel({
+  pause: "hover"
+})
+
+$('[id="myCarousel10"]').carousel({
+  interval: 5000
+})
+
+$('[id="myCarousel10"]').carousel({
+  pause: "hover"
+})
+
+// Activates Tooltips for Social Links
+$('.tooltip-social').tooltip({
+  selector: "a[data-toggle=tooltip]"
+})
